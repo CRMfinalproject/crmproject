b0f115049bd970495fe264576ceae3e490d8d1c4
Oleg Kustov <oleg0881@gmail.com>
2018-02-27 18:33:28 +0200

    Tablev2

--- src/js/data_table.js
+++ src/js/data_table.js
@@ -1,6 +1,7 @@
 const dataUrl = `https://trello-attachments.s3.amazonaws.com/5a7d77d8cb4876903b093cff/5a8d42f1e53d84d1cfc3dc11/74acb4c8bf49732b088038b596fb269e/data.json`;
 
-class table {
+
+class Table {
     constructor(data = {}) {
         this.fields =
             [
@@ -15,7 +16,7 @@ class table {
         this.renderTable(data);
     }
 
-    
+
 
     renderTable(data) {
 
@@ -36,27 +37,122 @@ class table {
         });
 
         containerTbl += `</tr></table></div>`;
-
+        document.body.innerHTML = containerTbl;
     }
 
 
 }
 
-function get() {
+// function get() {
 
-    fetch(dataUrl, { mode: 'no-cors', header: {'Access-Control-Allow-Origin': '*'} })
-        .then(response => {
-            if (response.ok) return response.json();
-            throw new Error("Error fetching data");
-        })
-        .then(data => {
-            new table(data);
-        })
-        .catch(error => {
-            console.error("Error: ", error);
-        });
+//     fetch(dataUrl, { mode: 'no-cors', header: {'Access-Control-Allow-Origin': '*'} })
+//         .then(response => {
+//             if (response.ok) return response.json();
+//             throw new Error("Error fetching data");
+//         })
+//         .then(data => {
+//             new table(data);
+//         })
+//         .catch(error => {
+//             console.error("Error: ", error);
+//         });
 
-}
-get();
+// }
+// //get();
 
+let data = [
+    {
+        name: "Помада Kylie",
+        category: "Косметика",
+        count: 259,
+        price: 299,
+        creationDate: "21/02/2018"
+    },
+    {
+        name: "Обезьянка",
+        category: "Игрушки",
+        count: 24,
+        price: 359,
+        creationDate: "20/02/2018"
+    },
+    {
+        name: "Кофта",
+        category: "Одежда",
+        count: 52,
+        price: 300,
+        creationDate: "13/02/2018"
+    },
+    {
+        name: "Кроссовки",
+        category: "Обувь",
+        count: 100,
+        price: 999,
+        creationDate: "12/02/2018"
+    },
+    {
+        name: "Библия",
+        category: "Книги",
+        count: 500,
+        price: 55,
+        creationDate: "19/02/2018"
+    },
+    {
+        name: "Мыло",
+        category: "Хоз.товары",
+        count: 500,
+        price: 15,
+        creationDate: "18/01/2018"
+    },
+    {
+        name: "Куртка",
+        category: "Кожа",
+        count: 5,
+        price: 5000,
+        creationDate: "21/02/2018"
+    },
+    {
+        name: "Ремень брючной",
+        category: "Ремни",
+        count: 10,
+        price: 199,
+        creationDate: "21/12/2017"
+    },
+    {
+        name: "Энциклопедия",
+        category: "Книги",
+        count: 200,
+        price: 150,
+        creationDate: "21/02/2018"
+    },
+    {
+        name: "Джинсы",
+        category: "Одежда",
+        count: 100,
+        price: 699,
+        creationDate: "01/02/2018"
+    },
+    {
+        name: "Лак",
+        category: "Косметика",
+        count: 1000,
+        price: 99,
+        creationDate: "21/02/2018"
+    },
+    {
+        name: "Кеды",
+        category: "Обувь",
+        count: 155,
+        price: 499,
+        creationDate: "11/02/2018"
+    },
+    {
+        name: "Шампунь",
+        category: "Хоз.товары",
+        count: 50,
+        price: 79,
+        creationDate: "21/02/2018"
+    },
+];
+
+new table(data);
 
