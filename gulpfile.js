248e8cff692173c4b14486b9844ced796166e25f
Ksenia Gromakovska <ksendows@gmail.com>
2018-03-06 14:03:59 +0200

    animation added

--- gulpfile.js
+++ gulpfile.js
@@ -135,7 +135,7 @@ gulp.task('server', () => {
       baseDir: "./dist",
       index: '/html/index.html'
     },
-    // browser: 'google chrome',
+    browser: 'google chrome',
     // Говорим спрятать надоедливое окошко обновления в браузере
     notify: false
   });
