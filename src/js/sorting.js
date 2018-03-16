741aab704a0406aeda1a1a3ada70ce028c4953f1
oshynkarenko <shynkarenko.o@gmail.com>
2018-03-16 18:08:01 +0200

    changed dataPage to data

--- src/js/sorting.js
+++ src/js/sorting.js
@@ -4,14 +4,14 @@ const sortByName = () => {
 	if(event.target.textContent === "Название товара"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				if (obj1.name < obj2.name) return 1;
 				if (obj1.name > obj2.name) return -1;
 				return 0;
 			});
 			renderData();
 		} else {
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				if (obj1.name < obj2.name) return -1;
 				if (obj1.name > obj2.name) return 1;
 				return 0;
@@ -26,14 +26,14 @@ const sortByCat = () => {
 	if(event.target.textContent === "Категория"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				if (obj1.category < obj2.category) return 1;
 				if (obj1.category > obj2.category) return -1;
 				return 0;
 			});
 			renderData();
 		} else {
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				if (obj1.category < obj2.category) return -1;
 				if (obj1.category > obj2.category) return 1;
 				return 0;
@@ -48,12 +48,12 @@ const sortByCount = () => {
 	if(event.target.textContent === "Кол-во на складе"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 			return obj2.count - obj1.count;
 			});
 			renderData();
 		} else {
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				return obj1.count - obj2.count;
 			});
 			renderData();
@@ -67,12 +67,12 @@ const sortByPrice = () => {
 	if(event.target.textContent === "Цена"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				return obj2.price - obj1.price;
 			});
 			renderData();
 		} else {
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				return obj1.price - obj2.price;
 			});
 			renderData();
@@ -85,12 +85,12 @@ const sortByDate = () => {
 	if(event.target.textContent === "Дата создания"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				return (new Date(obj2.creationDate) - new Date(obj1.creationDate));
 			});
 			renderData();
 		} else {
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				return (new Date(obj1.creationDate) - new Date(obj2.creationDate));
 			});
 			renderData();
@@ -103,12 +103,12 @@ const sortByWeight = () => {
 	if(event.target.textContent === "Вес"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				return obj2.weight - obj1.weight;
 			});
 			renderData();
 		} else {
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				return obj1.weight - obj2.weight;
 			});
 			renderData();
@@ -121,7 +121,7 @@ const sortBySize = () => {
 	if(event.target.textContent === "Размеры(ШхВхД)"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				let size1 = obj1.size.split("x");
 				let size2 = obj2.size.split("x");
 				let volume1 = size1[0] * size1[1] * size1[2];
@@ -130,7 +130,7 @@ const sortBySize = () => {
 			});
 			renderData();
 		} else {
-			dataPage.sort(function(obj1, obj2) {
+			data.sort(function(obj1, obj2) {
 				let size1 = obj1.size.split("x");
 				let size2 = obj2.size.split("x");
 				let volume1 = size1[0] * size1[1] * size1[2];
