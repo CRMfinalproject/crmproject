aa72b31bde70a09d7fc1c9e92996860b0a275f5b
Alex-Pobeda <34069629+Alex-Pobeda@users.noreply.github.com>
2018-03-16 16:19:21 +0200

    Updated to current version of dev

--- src/js/sorting.js
+++ src/js/sorting.js
@@ -1,40 +1,22 @@
 const tHead = document.querySelector(".data-table-header");
-const tBody = document.querySelector(".data-table-body");
-
-const updateView = products => {
-	let htmlString = "";
-	products.map(product => {
-		htmlString += `<tr class="row">
-				<td><input type="checkbox" name="checkbox"></td>
-				<td>${product.name}</td>
-				<td>${product.category}</td>
-				<td>${product.count}</td>
-				<td>${product.price}</td>
-				<td>${product.creationDate}</td>
-				<td>${product.weight}</td>
-				<td>${product.size}</td>
-				</tr>`;
-  });
-  tBody.innerHTML = htmlString;
-};
 
 const sortByName = () => {
 	if(event.target.textContent === "Название товара"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				if (obj1.name < obj2.name) return 1;
 				if (obj1.name > obj2.name) return -1;
 				return 0;
 			});
-			updateView(data);
+			renderData();
 		} else {
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				if (obj1.name < obj2.name) return -1;
 				if (obj1.name > obj2.name) return 1;
 				return 0;
 			});
-			updateView(data);
+			renderData();
 		}
 	}
 
@@ -44,116 +26,120 @@ const sortByCat = () => {
 	if(event.target.textContent === "Категория"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				if (obj1.category < obj2.category) return 1;
 				if (obj1.category > obj2.category) return -1;
 				return 0;
 			});
-			updateView(data);
+			renderData();
 		} else {
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				if (obj1.category < obj2.category) return -1;
 				if (obj1.category > obj2.category) return 1;
 				return 0;
 			});
-			updateView(data);
+			renderData();
 		}	
 	}
 };
+
 
 const sortByCount = () => {
 	if(event.target.textContent === "Кол-во на складе"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 			return obj2.count - obj1.count;
 			});
-			updateView(data);
+			renderData();
 		} else {
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				return obj1.count - obj2.count;
 			});
-			updateView(data);
+			renderData();
 		}
 	}
 	
 };
 
+
 const sortByPrice = () => {
 	if(event.target.textContent === "Цена"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				return obj2.price - obj1.price;
 			});
-			updateView(data);
+			renderData();
 		} else {
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				return obj1.price - obj2.price;
 			});
-			updateView(data);
+			renderData();
 		}
 	}
 };
+
 
 const sortByDate = () => {
 	if(event.target.textContent === "Дата создания"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				return (new Date(obj2.creationDate) - new Date(obj1.creationDate));
 			});
-			updateView(data);
+			renderData();
 		} else {
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				return (new Date(obj1.creationDate) - new Date(obj2.creationDate));
 			});
-			updateView(data);
+			renderData();
 		}
 	}
 };
+
 
 const sortByWeight = () => {
 	if(event.target.textContent === "Вес"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				return obj2.weight - obj1.weight;
 			});
-			updateView(data);
+			renderData();
 		} else {
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				return obj1.weight - obj2.weight;
 			});
-			updateView(data);
+			renderData();
 		}
 	}	
 };
+
 
 const sortBySize = () => {
 	if(event.target.textContent === "Размеры(ШхВхД)"){
 		event.target.classList.toggle("desc");
 		if(event.target.classList.contains("desc")){
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				let size1 = obj1.size.split("x");
 				let size2 = obj2.size.split("x");
 				let volume1 = size1[0] * size1[1] * size1[2];
 				let volume2 = size2[0] * size2[1] * size2[2];
 				return (volume2 - volume1);
 			});
-			updateView(data);
+			renderData();
 		} else {
-			data.sort(function(obj1, obj2) {
+			dataPage.sort(function(obj1, obj2) {
 				let size1 = obj1.size.split("x");
 				let size2 = obj2.size.split("x");
 				let volume1 = size1[0] * size1[1] * size1[2];
 				let volume2 = size2[0] * size2[1] * size2[2];
 				return volume1 - volume2;
 			});
-			updateView(data);
+			renderData();
 		}
 	}
-	
 };
 	
 tHead.addEventListener("click", function(){
