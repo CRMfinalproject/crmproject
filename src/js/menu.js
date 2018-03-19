869edf99669af3b0d388b76f2fe3ad027a3a87b1
Zhdaniuk Olena <e.mssixty@gmail.com>
2018-03-19 10:30:21 +0200

    commit

--- src/js/menu.js
+++ src/js/menu.js
@@ -79,3 +79,89 @@ burgerContMenu.element.addEventListener('mouseleave', event => {
 //         event.preventDefault();
 //     }
 // });
+
+class Menu {
+    constructor(selector, options) {
+        this.element = document.querySelector(selector);
+        this.subitems = document.querySelectorAll(selector + ` ${options.subItemClass}`); 
+        this.options = options;
+        this.element.addEventListener('click', event => this.onClick(event));
+        this.element.addEventListener('mouseleave', event => this.onItemMouseLeave(event));
+
+        this.subitems.forEach( subitem => subitem.addEventListener('click', event => this.onSubItemClick(event)));
+    }
+
+    onClick(event) {
+        this.prevent();
+
+        if (this.options.onClick)
+            this.options.onClick.call(this);
+    }
+
+    onSubItemClick(event) {
+        this.prevent();
+        this.close();
+
+        if (this.options.onSubItemClick)
+            this.options.onSubItemClick.call(this);
+    }
+
+    onItemMouseLeave(event) {
+        if (this.options.onItemMouseLeave)
+            this.options.onItemMouseLeave.call(this);
+    }
+
+    prevent() {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    toggle() {
+        this.element.classList.toggle(this.options.openStateClass);
+    }
+
+    open() {
+        this.element.classList.add(this.options.openStateClass);
+    }
+
+    hasOpenClass() {
+        return this.element.classList.contains(this.options.openStateClass);
+    }
+
+    close() {
+        this.element.classList.remove(this.options.openStateClass);
+    }
+}
+
+
+const userCabinetMenu = new Menu('.menu', {
+    openStateClass: 'menu__item--open',
+    subItemClass: '.menu__subitem',
+    onClick() {
+        console.log('userCabinetMenu: onClick')
+        this.open();
+    },
+    onSubItemClick() {
+        console.log('userCabinetMenu: onSubItemClick')
+    },
+    onItemMouseLeave() {
+        console.log('userCabinetMenu: onItemMouseLeave')
+        this.close();
+    }
+});
+
+const burgerBtn = new Menu('.burger', {
+    openStateClass: 'burger--open',
+    onClick() {
+        console.log('burgerBtn: onClick')
+        burgerMenu.toggle();
+    },
+});
+
+const burgerMenu = new Menu('.burger-menu__container', {
+    openStateClass: 'burger-menu__container--open',
+    onClick() {
+        console.log('burgerMenu: onClick')
+        this.close();
+    },
+});
