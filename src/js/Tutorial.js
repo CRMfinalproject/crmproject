1fff9f737da196adf0533aa70857246347cec898
Ksenia Gromakovska <ksendows@gmail.com>
2018-03-30 13:03:15 +0300

    checkbox unchecked if sorting

--- src/js/Tutorial.js
+++ src/js/Tutorial.js
@@ -3,13 +3,10 @@ export default class {
         debugger;
         this.contentHtml = contentHtml;
         this.sidebarHtml = document.querySelector('#js-tutorial-sidebar').textContent.trim();
-        this.triggerBtn = document.querySelector('#js-menu-tutorials');
-        this.triggerBtn.addEventListener('click', () => this.render());
     };
     render(){
         event.preventDefault();
         document.querySelector('.content').innerHTML = this.contentHtml + this.sidebarHtml;
-        // new Likecounter();
     }
 }
 
